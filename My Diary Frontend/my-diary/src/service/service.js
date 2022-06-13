ac573aef0ad91557e7c7ac1908475ad6779cc24a
Serhat Merak <42006836+merakserhat@users.noreply.github.com>
2022-06-13 17:08:27 +0300

    feat: Integrate axios package

---
+++ My Diary Frontend/my-diary/src/service/service.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import url from "url";
+
+const baseUrl = ""; //default localhost -> package.json proxy
+
+const api = axios.create({
+  baseUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const setAuthTokenToServiceInstance = (token) => {
+  if (token) {
+    api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  } else {
+    api.defaults.headers.common.Authorization = null;
+  }
+};
+
+async function makeRequest(method, pathname, data, options = {}) {
+  const body = method === "get" || !data ? {} : { data };
+  const requestUrl = url.format({ pathname, ...options });
+  const reqObj = {
+    method,
+    url: requestUrl,
+    ...body,
+    ...options,
+  };
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await api.request(reqObj);
+      resolve(res.data);
+    } catch (error) {
+      if (error.response) {
+        reject(error?.response?.data);
+      }
+      reject(error);
+    }
+  });
+}
+
+const Service = {
+  get(path, options) {
+    return makeRequest("GET", path, null, options);
+  },
+
+  post(path, data, options) {
+    return makeRequest("POST", path, data, options);
+  },
+
+  put(path, data, options) {
+    return makeRequest("PUT", path, data, options);
+  },
+
+  delete(path, data, options) {
+    return makeRequest("DELETE", path, data, options);
+  },
+};
+
+export default Service;
